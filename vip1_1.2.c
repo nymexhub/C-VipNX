be917d3862fa660ba924547f31c1de5747ef9729
Felipe <f.alfonso@res-ear.ch>
2023-04-29 08:44:29 -0400

    updates

--- vip1_1.2.c
+++ vip1_1.2.c
@@ -4,8 +4,8 @@
  *************************************
  * gunzip vip.c.gz
  * gcc -o vip vip.c (compilation)
- * su
- * vi /etc/group (nuevo grupo, ej.: staff:x:100:user)
+ * su or sudo 
+ * vim /etc/group (nuevo grupo, ej.: staff:x:100:user)
  * mv vip /usr/bin/
  * exit
  * vip
